638276a5fd421ba6bdb3a5f65eb1c0008f9b5888
Stephen Haywood <shaywood@tenable.com>
2015-03-16 12:12:31 -0400

    Very basic starter script.

---
+++ nessus6_basic.py
@@ -0,0 +1,84 @@
+import requests
+import json
+import sys
+
+# Disable Warning when not verifying SSL certs.
+requests.packages.urllib3.disable_warnings()
+
+
+url = 'https://<nessus_ip_or_hostname>:8834'
+verify = False
+token = ''
+username = 'admin'
+password = 'password'
+
+
+def build_url(resource):
+    return '{0}{1}'.format(url, resource)
+
+
+def connect(method, resource, data=None):
+    """
+    Send a request
+
+    Send a request to Nessus based on the specified data. If the session token
+    is available add it to the request. Specify the content type as JSON and
+    convert the data to JSON format.
+    """
+    headers = {'X-Cookie': 'token={0}'.format(token),
+               'content-type': 'application/json'}
+
+    data = json.dumps(data)
+
+    if method == 'POST':
+        r = requests.post(build_url(resource), data=data, headers=headers, verify=verify)
+    elif method == 'PUT':
+        r = requests.put(build_url(resource), data=data, headers=headers, verify=verify)
+    elif method == 'DELETE':
+        r = requests.delete(build_url(resource), data=data, headers=headers, verify=verify)
+    else:
+        r = requests.get(build_url(resource), params=data, headers=headers, verify=verify)
+
+    # Exit if there is an error.
+    if r.status_code != 200:
+        e = r.json()
+        print e['error']
+        sys.exit()
+
+    # When downloading a scan we need the raw contents not the JSON data. 
+    if 'download' in resource:
+        return r.content
+    
+    # All other responses should be JSON data. Return raw content if they are
+    # not.
+    try:
+        return r.json()
+    except ValueError:
+        return r.content
+
+
+def login(usr, pwd):
+    """
+    Login to nessus.
+    """
+
+    login = {'username': usr, 'password': pwd}
+    data = connect('POST', '/session', data=login)
+
+    return data['token']
+
+
+def logout():
+    """
+    Logout of nessus.
+    """
+
+    connect('DELETE', '/session')
+
+
+if __name__ == '__main__':
+    print('Login')
+    token = login(username, password)
+    print('Token: {0}'.format(token))
+    print('Logout')
+    logout()
